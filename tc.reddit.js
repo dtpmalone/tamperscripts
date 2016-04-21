a8733c59d60b9fb6dc0c92bd5f57ae62fe1f8218
Daniel Malone <dtp.malone@gmail.com>
2016-04-21 05:09:01 +0200

    added post title styling

--- tc.reddit.js
+++ tc.reddit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tc.reddit
 // @namespace    com.tangledcode
-// @version      0.1
+// @version      0.2
 // @description  visual aid for the mobil reddit page
 // @author       Daniel Malone
 // @match        https://m.reddit.com/*
@@ -10,3 +10,4 @@
 // ==/UserScript==
 
 GM_addStyle('.Listing { width: 800px; margin: auto; }');
+GM_addStyle('PostHeader__post-title-line { font-size: 20px; font-weight: bold; }');
