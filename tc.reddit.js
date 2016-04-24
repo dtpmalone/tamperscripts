c47fa6d7716f382e5025ce6231d1db545c7c7440
Daniel Malone <dtp.malone@gmail.com>
2016-04-24 21:21:38 +0200

    changed color and size of next-page button

--- tc.reddit.js
+++ tc.reddit.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         tc.reddit
 // @namespace    com.tangledcode
-// @version      0.2
+// @version      0.3
 // @description  visual aid for the mobil reddit page
 // @author       Daniel Malone
 // @match        https://m.reddit.com/*
 // @grant        GM_addStyle
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // ==/UserScript==
 
 GM_addStyle('.Listing { width: 800px; margin: auto; }');
 GM_addStyle('PostHeader__post-title-line { font-size: 20px; font-weight: bold; }');
+GM_addStyle('.IndexPage-button.next { width: 800px; margin-right: -17px; margin-top: 50px; margin-bottom: 150px; text-align: center; padding: 20px; color: #fff; background-color: #449d44; border-color: #398439; }');
