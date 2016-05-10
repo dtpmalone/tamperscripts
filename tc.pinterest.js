c7cc14fc9c15ba083c63c20ae115de215cb2b11e
Daniel Malone <dtp.malone@gmail.com>
2016-05-10 15:55:27 +0200

    Create tc.pinterest.js

---
+++ tc.pinterest.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         tc.pinterest
+// @namespace    com.tangledcode
+// @version      0.1
+// @description  visual aid for pinterest
+// @author       Daniel Malone
+// @match        https://de.pinterest.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle('.Pin.summary .creditFooter { color: #fff; background-color: #c9302c; border-color: #ac2925; }');
