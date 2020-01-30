1e305f2f65b0e3d001a5f2f3a75715f15a6dd925
Daniel Malone <dtp.malone@gmail.com>
2020-01-30 15:41:42 +0100

    updates tc.tweetdeck.js
    - increased max-height for enlargened image views
    - removes text and background from enlargened image view

--- tc.tweetdeck.js
+++ tc.tweetdeck.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tc.tweetdeck
 // @namespace    com.tangledcode
-// @version      0.3
+// @version      0.4
 // @updateURL    https://raw.githubusercontent.com/dtpmalone/tamperscripts/master/tc.tweetdeck.js
 // @description  visual aid for TweetDeck
 // @author       Daniel Malone
@@ -34,6 +34,12 @@ $(document).ready(() => {
   GM_addStyle('.js-tweet-text .link-complex[rel="user"], .js-quoted-tweet-text .link-complex[rel="user"] { color: #fff; background-color: #1da1f2; border-radius: 5px; padding: 3px 8px; line-height: 1.8rem; }')
   GM_addStyle('.js-tweet-body { margin: 10px 0; }')
   GM_addStyle('.item-box { padding: 20px 60px; }')
+  GM_addStyle('.js-embeditem { height: 1200px; top :0; }')
+  GM_addStyle('.js-med-tweet { display: none; }')
+  GM_addStyle('.js-modal-panel { display: border: 0; background: transparent; box-shadow: none; }')
+  js-modal-panel
+
+
 
   replaceMediaImages()
 })
