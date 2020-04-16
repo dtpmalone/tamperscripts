e61306d79fc82109bd62a8969be64efd2520811d
Daniel Malone <dtp.malone@gmail.com>
2020-04-16 23:18:14 +0200

    creates tc.dndbeyond.js

---
+++ tc.dndbeyond.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         tc.dndbeyond
+// @namespace    com.tangledcode
+// @version      0.4
+// @updateURL    https://raw.githubusercontent.com/dtpmalone/tamperscripts/master/tc.dndbeyond.js
+// @description  visual aid for D&D Beyond
+// @author       Daniel Malone
+// @match        https://www.dndbeyond.com/*
+// @grant        GM_addStyle
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle('.content-unlocked { background-color: red; padding: 4px 6px; border-radius: 6px; color: white !important; }')
+})();
