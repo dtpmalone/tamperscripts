608f93191854f87caf3d5c0e32b5d44c9bbabafe
Daniel Malone <dtp.malone@gmail.com>
2020-04-18 01:27:57 +0200

    creates tc.dndbeyond-marketplace.js

---
+++ tc.dndbeyond-marketplace.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         tc.dndbeyond
+// @namespace    com.tangledcode
+// @version      0.1
+// @updateURL    https://raw.githubusercontent.com/dtpmalone/tamperscripts/master/tc.dndbeyond-marketplace.js
+// @description  visual aid for D&D Beyond | Marketplace
+// @author       Daniel Malone
+// @match        https://www.dndbeyond.com/marketplace/*
+// @grant        GM_addStyle
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // content labeling
+  GM_addStyle('.content-unlocked { background-color: #47d18c; padding: 2px 8px; border-radius: 6px; color: #242527 !important; }');
+})();
