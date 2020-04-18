b4db25f443497bd577f73f7d73b9df087955d840
Daniel Malone <dtp.malone@gmail.com>
2020-04-18 05:02:04 +0200

    updates tc.dndbeyond-combat-tracker.js
    - removes jQuery
    - corrects formatting

--- tc.dndbeyond-combat-tracker.js
+++ tc.dndbeyond-combat-tracker.js
@@ -7,14 +7,13 @@
 // @author       Daniel Malone
 // @match        https://www.dndbeyond.com/combat-tracker/*
 // @grant        GM_addStyle
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
   'use strict';
 
   // character labeling
-  GM_addStyle('.container .combatant-card .combatant-summary__details:focus { outline: 0!important; }');
+  GM_addStyle('.container .combatant-card .combatant-summary__details: focus { outline: 0!important; }');
 
   GM_addStyle('.container .combatant-card .combatant-card__left-bit--short { border: none; border-left-width: 6px; border-left-style: solid; border-top-left-radius: 6px; border-bottom-left-radius: 6px; width: 55px; }');
   GM_addStyle('.container .combatant-card .combatant-card__mid-bit { border-bottom-left-radius: 6px; border-right-width: 0!important; }');
@@ -33,16 +32,16 @@
   GM_addStyle('.container .combatant-card--character.in-combat .combatant-card__right-bit { border-color: #ddd; border-right-color: #242527!important; background-color: #ddd; }');
 
   // full screen
-  GM_addStyle('@media screen and (min-width:1025px) { body.responsive-enabled #site #site-main .container { max-width:1800px!important; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker__combatants { max-height:fit-content; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__body { padding:0; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__combat-tracker { max-height:85vh; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__content-section { overflow:hidden; max-height:85vh; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__content-section:hover { overflow:auto overlay; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { #footer { display:none; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { body.responsive-enabled #site #site-main .container { max-width: 1800px!important; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker__combatants { max-height: fit-content; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__body { padding: 0; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__combat-tracker { max-height: 85vh; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__content-section { overflow: hidden; max-height: 85vh; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__content-section: hover { overflow: auto overlay; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { #footer { display: none; } }');
 
   // 3 columns
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__combat-tracker { width:35%; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__content { width:75%; display:flex; flex-direction:row; flex-wrap:nowrap; justify-content:space-between; align-items:flex-start; align-content:flex-start; } }');
-  GM_addStyle('@media screen and (min-width:1025px) { .container .combat-tracker-page__content section:not(:first-child) { margin-left:24px; margin-top:0; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__combat-tracker { width: 35%; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__content { width: 75%; display: flex; flex-direction: row; flex-wrap: nowrap; justify-content: space-between; align-items: flex-start; align-content: flex-start; } }');
+  GM_addStyle('@media screen and (min-width: 1025px) { .container .combat-tracker-page__content section: not(: first-child) { margin-left: 24px; margin-top: 0; } }');
 })();
