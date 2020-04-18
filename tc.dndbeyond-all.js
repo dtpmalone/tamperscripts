1c12dcde9ab342e5dd3ac76e6703ba091519be01
Daniel Malone <dtp.malone@gmail.com>
2020-04-18 05:15:12 +0200

    updates tc.dndbeyond-all.js
    - widens base scrollbar
    - corrects formatting
    - increments version

--- tc.dndbeyond-all.js
+++ tc.dndbeyond-all.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tc.dndbeyond-all
 // @namespace    com.tangledcode
-// @version      0.1
+// @version      0.2
 // @updateURL    https://raw.githubusercontent.com/dtpmalone/tamperscripts/master/tc.dndbeyond-all.js
 // @description  visual aid for D&D Beyond | All Pages
 // @author       Daniel Malone
@@ -14,8 +14,10 @@
   'use strict';
 
   // scrollbars
-  GM_addStyle('@media screen and (min-width:1025px){*::-webkit-scrollbar{width:4px;}}');
-  GM_addStyle('@media screen and (min-width:1025px){*::-webkit-scrollbar-track{background-color:#f1f1f1;}}');
-  GM_addStyle('@media screen and (min-width:1025px){*::-webkit-scrollbar-thumb{background-color:#c1c1c1;}}');
-  GM_addStyle('@media screen and (min-width:1025px){*::-webkit-scrollbar-thumb:hover{background-color:#a9a9a9;}}');
+  GM_addStyle('*::-webkit-scrollbar { width: 4px; }');
+  GM_addStyle('*::-webkit-scrollbar-track { background-color: #f1f1f1; }');
+  GM_addStyle('*::-webkit-scrollbar-thumb { background-color: #c1c1c1; }');
+  GM_addStyle('*::-webkit-scrollbar-thumb:hover { background-color: #a9a9a9; }');
+
+  GM_addStyle('body::-webkit-scrollbar { width: 8px; }');
 })();
