62b90a74982f96ff59083bdfe6712a407269ab50
Daniel Malone <dtp.malone@gmail.com>
2020-11-04 10:05:18 +0100

    updates tc.legendkeeper.js
    aligns bullet point text left

--- tc.legendkeeper.js
+++ tc.legendkeeper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tc.legendkeeper
 // @namespace    com.tangledcode
-// @version      0.1
+// @version      0.2
 // @updateURL    https://raw.githubusercontent.com/dtpmalone/tamperscripts/master/tc.legendkeeper.js
 // @description  visual aid for LegendKeeper
 // @author       Daniel Malone
@@ -9,12 +9,13 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('.theme-dark .lk-mention { color: #42B983; border-bottom: .2em solid #c49454; }');
-GM_addStyle('u { text-decoration: none; color: #42B983; font-weight: bold; }');
+GM_addStyle('.theme-dark .lk-mention { color: #42b983; border-bottom: .2em solid #c49454; }');
+GM_addStyle('u { text-decoration: none; color: #42b983; font-weight: bold; }');
 GM_addStyle('a { font-weight: bold; }');
-GM_addStyle('s { text-decoration: none; color: #476582; background-color: #E8EBEC; border-radius: 3px; padding: .25rem .5rem; margin: 0; }');
-GM_addStyle('sup { top: 0; font-size: 100%; color: #42B983; }');
+GM_addStyle('s { text-decoration: none; color: #476582; background-color: #e8ebec; border-radius: 3px; padding: .25rem .5rem; margin: 0; }');
+GM_addStyle('sup { top: 0; font-size: 100%; color: #42b983; }');
 GM_addStyle('sub { top: 0; font-size: 100%; color: #e71d36; }');
+GM_addStyle('.theme-dark .lk-editor .ProseMirror p { text-align: revert; }');
 
 // highlight external links
 GM_addStyle('a[href^="https://www.dndbeyond.com/"] { border-bottom: .2em solid #e40712; }');
